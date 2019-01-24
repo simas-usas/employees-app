9bad6d9e276b7247ee2165a306898443f09b049d
simas-usas <simas.s.usas@gmail.com>
2019-01-24 10:05:34 +0200

    Added sort to tabs.

--- app/src/app/selectors/tabs.js
+++ app/src/app/selectors/tabs.js
@@ -1,6 +1,10 @@
 export default (employees, tabs) => {
-  return employees.filter((employee) => {
-    const idMatch = tabs.includes(employee.id);
-    return idMatch;
-  })
+  return employees
+    .filter((employee) => {
+      const idMatch = tabs.includes(employee.id);
+      return idMatch;
+    })
+    .sort((employee, tab) => {
+      return tabs.indexOf(employee) - employees.indexOf(tab);
+    })
 };
